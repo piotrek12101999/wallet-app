c2873a708f2923ebf80851cd1df3b6198f52fbae
Piotr <piter.gazownik@o2.pl>
2019-11-17 15:36:27 +0100

    libraries added

---
+++ src/env/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB-81Z5ZFWfB0MW2J-aF2nPypiakqNy4VI',
+  authDomain: 'domi-app-ef173.firebaseapp.com',
+  databaseURL: 'https://domi-app-ef173.firebaseio.com',
+  projectId: 'domi-app-ef173',
+  storageBucket: 'domi-app-ef173.appspot.com',
+  messagingSenderId: '964190272449',
+  appId: '1:964190272449:web:7f40bc2699a813d921cd40'
+};
